56053c970b2575caa6d4e7505255bf2723854cd6
Seth Pacheco <seth.pacheco@gmail.com>
2018-03-23 01:45:29 -0600

    Add files via upload

---
+++ playDate/routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  res.send('respond with a resource');
+});
+
+/* Test GET */
+router.get('/cool', (req, res, next) => {
+	res.send("You're so cool");
+});
+
+module.exports = router;
